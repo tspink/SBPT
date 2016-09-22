e75c45b0f4f5a52ff1fe7a1bd68f4670aaf65044
Tom Spink <tspink@gmail.com>
2016-09-22 11:54:52 +0100

    Count kernel routine invocations

--- SBPT.cpp
+++ SBPT.cpp
@@ -3,6 +3,22 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <list>
+
+struct KernelRoutineDescriptor
+{
+	KernelRoutineDescriptor() : count(0) { }
+	
+	uint64_t count;
+	std::string name;
+};
+
+std::list<KernelRoutineDescriptor *> KernelRoutineDescriptors;
+
+void KernelRoutineEnter(KernelRoutineDescriptor *descriptor)
+{
+	descriptor->count++;
+}
 
 void Routine(RTN rtn, void *v)
 {
@@ -12,6 +28,27 @@ void Routine(RTN rtn, void *v)
 	if (SEC_Name(sec) != ".kernel") return;
 	
 	std::cerr << "Identified kernel routine: " << RTN_Name(rtn) << std::endl;
+	
+	KernelRoutineDescriptor *descriptor = new KernelRoutineDescriptor();
+	descriptor->name = RTN_Name(rtn);
+	KernelRoutineDescriptors.push_back(descriptor);
+	
+	RTN_Open(rtn);
+	RTN_InsertCall(rtn, IPOINT_BEFORE, (AFUNPTR)KernelRoutineEnter, IARG_PTR, descriptor, IARG_END);
+	RTN_Close(rtn);
+}
+
+void Fini(INT32 code, void *v)
+{
+	std::cerr << std::endl;
+	std::cerr << "*** SLAMBeanch Completed ***" << std::endl;
+	
+	for (auto descriptor : KernelRoutineDescriptors) {
+		std::cerr << "Kernel: " << descriptor->name << ", Count: " << descriptor->count << std::endl;
+		delete descriptor;
+	}
+	
+	KernelRoutineDescriptors.clear();
 }
 
 int main(int argc, char *argv[])
@@ -27,6 +64,7 @@ int main(int argc, char *argv[])
 	}
 	
 	RTN_AddInstrumentFunction(Routine, NULL);
+	PIN_AddFiniFunction(Fini, NULL);
 
 	PIN_StartProgram();
 	return 0;
