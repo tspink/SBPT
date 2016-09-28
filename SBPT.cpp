08f1a6a70f754d51a90a621be42f9cad71128bae
Tom Spink <tspink@gmail.com>
2016-09-28 13:11:01 +0100

    Introduce memory re-use tracking

--- SBPT.cpp
+++ SBPT.cpp
@@ -7,10 +7,11 @@
 #include <iomanip>
 #include <fstream>
 #include <list>
+#include <map>
+#include <unordered_map>
 #include <time.h>
 
 KNOB<bool> KnobTraceMemory(KNOB_MODE_WRITEONCE, "pintool", "trace_mem", "0", "Should trace memory");
-KNOB<string> KnobTraceFile(KNOB_MODE_WRITEONCE, "pintool", "trace_file", "trace.bin", "Trace file");
 
 static uint64_t now()
 {
@@ -20,10 +21,36 @@ static uint64_t now()
 	return tv.tv_sec * 1e6 + tv.tv_usec;
 }
 
-struct MemoryAccess
+struct Average
 {
-	uint64_t Address;
-	bool Read;
+	Average() : Value(0), DataPoints(0) { }
+	
+	void Add(double new_value) {
+		if (!DataPoints) {
+			Value = new_value;
+		} else {
+			Value = Value * ((double)(DataPoints - 1) / (double)DataPoints) + (new_value / DataPoints);
+		}
+		
+		DataPoints++;
+	}
+	
+	double Value;
+	uint64_t DataPoints;
+};
+
+struct MemoryZone
+{
+	uint64_t TotalReads, TotalWrites;
+	std::unordered_map<uint64_t, uint64_t> AddressAccesses, AddressReads, AddressWrites;
+
+	Average AverageReuse, AverageReuseDistance;
+	uint64_t MaxReuseDistance;
+};
+
+struct MemoryStatistics
+{
+	MemoryZone StackZone, HeapZone, DataZone;	
 };
 
 struct KernelDescriptor
@@ -42,8 +69,6 @@ struct KernelInvocation
 	
 	KernelDescriptor *Descriptor;
 	uint64_t Duration;
-	
-	std::list<MemoryAccess> MemoryAccessTrace;
 };
 
 struct FrameDescriptor
@@ -53,47 +78,27 @@ struct FrameDescriptor
 	uint64_t Duration;
 };
 
+#define VMA_TYPE_DATA  0
+#define VMA_TYPE_STACK 1
+#define VMA_TYPE_HEAP  2
+
+struct VMA
+{
+	uint64_t Start, End;
+	uint8_t Type;
+};
+
 std::list<KernelDescriptor *> KernelDescriptors;
 std::list<FrameDescriptor *> FrameDescriptors;
+std::map<uint64_t, VMA> VMAs;
+
+static MemoryStatistics MemoryStats;
 
 static FrameDescriptor *CurrentFrame;
 static KernelInvocation *CurrentKernel;
 static int NextKernelID;
 
-static int LogFD, CurrentFrameIndex;
-
-#define LOG_PACKET_FRAME_START	0
-#define LOG_PACKET_FRAME_END	1
-#define LOG_PACKET_KERNEL_START	2
-#define LOG_PACKET_KERNEL_END	3
-#define LOG_PACKET_MEM_READ		4
-#define LOG_PACKET_MEM_WRITE	5
-
-struct LogFramePacket
-{
-	uint8_t type;
-	uint64_t time;
-	uint32_t index;
-} __attribute__((packed));
-
-struct LogKernelPacket
-{
-	uint8_t type;
-	uint64_t time;
-	uint32_t kernel;
-} __attribute__((packed));
-
-struct LogMemPacket
-{
-	uint8_t type;
-	uint64_t time;
-	uint64_t address;
-} __attribute__((packed));
-
-static uint64_t CurrentTime()
-{
-	return 0;
-}
+static int CurrentFrameIndex;
 
 void FrameStart()
 {
@@ -102,25 +107,11 @@ void FrameStart()
 	CurrentFrame = new FrameDescriptor();
 	CurrentFrame->Index = CurrentFrameIndex++;
 	CurrentFrame->Duration = now();
-	
-	LogFramePacket lfp;
-	lfp.type = LOG_PACKET_FRAME_START;
-	lfp.time = CurrentTime();
-	lfp.index = CurrentFrame->Index;
-	
-	write(LogFD, &lfp, sizeof(lfp));
 }
 
 void FrameEnd()
 {
 	ASSERT(CurrentFrame, "A frame is not in progress");
-	
-	LogFramePacket lfp;
-	lfp.type = LOG_PACKET_FRAME_END;
-	lfp.time = CurrentTime();
-	lfp.index = CurrentFrame->Index;
-	
-	write(LogFD, &lfp, sizeof(lfp));
 	
 	CurrentFrame->Duration = now() - CurrentFrame->Duration;
 	
@@ -135,26 +126,12 @@ void KernelRoutineEnter(KernelDescriptor *descriptor)
 
 	CurrentKernel = new KernelInvocation(descriptor);
 	CurrentKernel->Duration = now();
-	
-	LogKernelPacket lfp;
-	lfp.type = LOG_PACKET_KERNEL_START;
-	lfp.time = CurrentTime();
-	lfp.kernel = descriptor->ID;
-	
-	write(LogFD, &lfp, sizeof(lfp));
 }
 
 void KernelRoutineExit(KernelDescriptor *descriptor)
 {
 	ASSERT(CurrentFrame, "A frame is not in progress");
 	ASSERT(CurrentKernel, "A kernel is not in progress");
-	
-	LogKernelPacket lfp;
-	lfp.type = LOG_PACKET_KERNEL_END;
-	lfp.time = CurrentTime();
-	lfp.kernel = descriptor->ID;
-	
-	write(LogFD, &lfp, sizeof(lfp));
 	
 	CurrentKernel->Duration = now() - CurrentKernel->Duration;
 	CurrentFrame->KernelInvocations.push_back(CurrentKernel);
@@ -164,24 +141,75 @@ void KernelRoutineExit(KernelDescriptor *descriptor)
 	CurrentKernel = NULL;
 }
 
+static uintptr_t ReuseQueue[4096];
+static uint64_t ReuseQueueSize, LastTimepoint;
+
+void MemoryAccessCommon(uintptr_t addr, MemoryZone& zone)
+{
+	bool found = false;
+	unsigned int index;
+	for (index = 0; index < ReuseQueueSize; index++) {
+		if (ReuseQueue[index] == addr) {
+			found = true;
+			break;
+		}
+	}
+	
+	if (found) {
+		uint64_t distance = ReuseQueueSize - index;
+		if (distance > zone.MaxReuseDistance)
+			zone.MaxReuseDistance = distance;
+
+		zone.AverageReuseDistance.Add(distance);
+		ReuseQueueSize = 0;
+	} else {
+		ReuseQueue[ReuseQueueSize++] = addr;
+		if (ReuseQueueSize >= 4096) ReuseQueueSize = 0;
+	}
+	
+	zone.AddressAccesses[addr]++;
+	
+	if (((zone.TotalWrites + zone.TotalReads) % 1048576) == 0) {
+		uint64_t delta = now() - LastTimepoint;
+		std::cerr << "Processed " << std::dec << (zone.TotalWrites + zone.TotalReads) << " accesses (" << (uint64_t)(1000000.0 / (delta / 1e6)) << " APS)" << std::endl;
+		LastTimepoint = now();
+	}
+}
+
+static inline MemoryZone& ClassifyAddress(uintptr_t addr)
+{
+	auto vma = VMAs.lower_bound(addr);
+	if (vma == VMAs.end())
+		return MemoryStats.HeapZone;
+	
+	if (addr < vma->second.End) {
+		if (vma->second.Type == VMA_TYPE_STACK)
+			return MemoryStats.StackZone;
+		else
+			return MemoryStats.DataZone;
+	} else {
+		return MemoryStats.HeapZone;
+	}
+}
+
 void MemoryReadInstruction(void *rip, uintptr_t addr)
 {
-	LogMemPacket lmp;
-	lmp.type = LOG_PACKET_MEM_READ;
-	lmp.time = CurrentTime();
-	lmp.address = addr;
+	MemoryZone& zone = ClassifyAddress(addr);
 	
-	write(LogFD, &lmp, sizeof(lmp));
+	zone.TotalReads++;
+	zone.AddressReads[addr]++;
+	
+	MemoryAccessCommon(addr, zone);
 }
 
 void MemoryWriteInstruction(void *rip, uintptr_t addr)
-{	
-	LogMemPacket lmp;
-	lmp.type = LOG_PACKET_MEM_WRITE;
-	lmp.time = CurrentTime();
-	lmp.address = addr;
+{
+	MemoryZone& zone = ClassifyAddress(addr);
 	
-	write(LogFD, &lmp, sizeof(lmp));
+	zone.TotalWrites++;
+	zone.AddressWrites[addr]++;
+	
+	MemoryAccessCommon(addr, zone);
 }
 
 void Routine(RTN rtn, VOID *v)
@@ -234,11 +262,54 @@ void Instruction(INS ins, VOID *p)
 
 void Fini(INT32 code, void *v)
 {
-	close(LogFD);
-	
 	std::cerr << std::endl;
 	std::cerr << "*** SLAMBench Completed ***" << std::endl;
 	
+	std::cerr << "Memory Statistics:" << std::endl;
+	
+	MemoryZone& zone1 = MemoryStats.DataZone;
+	std::cerr << "*** DATA ***" << std::dec << std::endl;
+	std::cerr << "        Total Accesses: Reads=" << zone1.TotalReads << ", Writes=" << zone1.TotalWrites << ", Total=" << (zone1.TotalReads + zone1.TotalWrites) << std::endl;
+	std::cerr << "     Distinct Accesses: Reads=" << zone1.AddressReads.size() << ", Writes=" << zone1.AddressWrites.size() << ", Total=" << (zone1.AddressReads.size() + zone1.AddressWrites.size()) << std::endl;
+	
+	std::cerr << "Average Reuse Distance: " << zone1.AverageReuseDistance.Value << std::endl;
+	std::cerr << "   Max. Reuse Distance: " << zone1.MaxReuseDistance << std::endl;
+
+	for (auto access : zone1.AddressAccesses) {
+		zone1.AverageReuse.Add(access.second);
+	}
+
+	std::cerr << "         Average Reuse: " << zone1.AverageReuse.Value << std::endl << std::endl;
+
+	MemoryZone& zone2 = MemoryStats.StackZone;
+	std::cerr << "*** STACK ***" << std::endl;
+	std::cerr << "        Total Accesses: Reads=" << zone2.TotalReads << ", Writes=" << zone2.TotalWrites << ", Total=" << (zone2.TotalReads + zone2.TotalWrites) << std::endl;
+	std::cerr << "     Distinct Accesses: Reads=" << zone2.AddressReads.size() << ", Writes=" << zone2.AddressWrites.size() << ", Total=" << (zone2.AddressReads.size() + zone2.AddressWrites.size()) << std::endl;
+	
+	std::cerr << "Average Reuse Distance: " << zone2.AverageReuseDistance.Value << std::endl;
+	std::cerr << "   Max. Reuse Distance: " << zone2.MaxReuseDistance << std::endl;
+
+	for (auto access : zone2.AddressAccesses) {
+		zone2.AverageReuse.Add(access.second);
+	}
+
+	std::cerr << "         Average Reuse: " << zone2.AverageReuse.Value << std::endl << std::endl;
+
+	MemoryZone& zone3 = MemoryStats.HeapZone;
+	std::cerr << "*** HEAP ***" << std::endl;
+	std::cerr << "        Total Accesses: Reads=" << zone3.TotalReads << ", Writes=" << zone3.TotalWrites << ", Total=" << (zone3.TotalReads + zone3.TotalWrites) << std::endl;
+	std::cerr << "     Distinct Accesses: Reads=" << zone3.AddressReads.size() << ", Writes=" << zone3.AddressWrites.size() << ", Total=" << (zone3.AddressReads.size() + zone3.AddressWrites.size()) << std::endl;
+	
+	std::cerr << "Average Reuse Distance: " << zone3.AverageReuseDistance.Value << std::endl;
+	std::cerr << "   Max. Reuse Distance: " << zone3.MaxReuseDistance << std::endl;
+
+	for (auto access : zone3.AddressAccesses) {
+		zone3.AverageReuse.Add(access.second);
+	}
+
+	std::cerr << "         Average Reuse: " << zone3.AverageReuse.Value << std::endl << std::endl;
+	
+#if 0
 	uint64_t all_kernel_executions = 0;
 	for (auto descriptor : KernelDescriptors) {
 		all_kernel_executions += descriptor->TotalExecutionCount;
@@ -314,7 +385,99 @@ void Fini(INT32 code, void *v)
 		
 		std::cerr << std::endl;
 	}
+#endif
 }
+
+static void FindStack()
+{
+	FILE *maps = fopen("/proc/self/maps", "rt");
+	if (!maps) {
+		return;
+	}
+
+	uint64_t rsp;
+	asm volatile("mov %%rsp, %0" : "=r"(rsp));
+
+	while (!feof(maps)) {
+		char buffer[512];
+		fgets(buffer, sizeof(buffer) - 1, maps);
+
+		VMA vma;
+		sscanf(buffer, "%lx-%lx", &vma.Start, &vma.End);
+
+		if (rsp >= vma.Start && rsp < vma.End) {
+			std::cerr << "FOUND STACK: " << std::hex << vma.Start << "--" << vma.End << std::endl;
+			vma.Type = VMA_TYPE_STACK;
+			VMAs[vma.Start] = vma;
+			break;
+		}
+	}
+
+	fclose(maps);
+}
+
+void Image(IMG img, VOID *v)
+{
+	std::cerr << "IMAGE: " << IMG_Name(img) << std::endl;
+	
+	for (SEC sec = IMG_SecHead(img); SEC_Valid(sec); sec = SEC_Next(sec)) {
+		if (!SEC_Mapped(sec)) continue;
+		
+		std::cerr << "  SECTION: " << SEC_Name(sec) << std::endl;
+		std::cerr << "  START:" << SEC_Address(sec) << ", SIZE:" << SEC_Size(sec) << std::endl;
+		
+		VMA vma;
+		vma.Start = SEC_Address(sec);
+		vma.End   = vma.Start + SEC_Size(sec);
+		vma.Type  = VMA_TYPE_DATA;
+		
+		VMAs[SEC_Address(sec)] = vma;
+	}
+	
+	FindStack();
+}
+
+//static void LoadVMAs()
+//{
+//	FILE *maps = fopen("/proc/self/maps", "rt");
+//	if (!maps) {
+//		return;
+//	}
+//
+//	uint64_t rsp;
+//	asm volatile("mov %%rsp, %0" : "=r"(rsp));
+//
+//	while (!feof(maps)) {
+//		char buffer[512];
+//		fgets(buffer, sizeof(buffer) - 1, maps);
+//
+//		VMA vma;
+//		sscanf(buffer, "%lx-%lx", &vma.Start, &vma.End);
+//
+//		if (strstr(buffer, "[stack]")) {
+//			vma.Type = 0;
+//		} else if (strstr(buffer, "[heap]")) {
+//			vma.Type = 1;
+//		} else {
+//			vma.Type = 2;
+//		}
+//
+//		if (rsp >= vma.Start && rsp < vma.End) {
+//			vma.Type = 0;
+//		}
+//
+//		VMAs[vma.Start] = vma;
+//
+//		fprintf(stderr, "MAP: %s", buffer);
+//		//fprintf(stderr, "Adding VMA %lx -- %lx (%d)\n", vma.Start, vma.End, vma.Type);
+//
+//		if (vma.Type == 0) {
+//			StackVMA = vma;
+//		}
+//	}
+//
+//	fclose(maps);
+//}
 
 int main(int argc, char *argv[])
 {
@@ -328,19 +491,15 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	
+	IMG_AddInstrumentFunction(Image, NULL);
+	
 	RTN_AddInstrumentFunction(Routine, NULL);
 	
 	if (KnobTraceMemory.Value())
 		INS_AddInstrumentFunction(Instruction, NULL);
 	
 	PIN_AddFiniFunction(Fini, NULL);
-
-	LogFD = open(KnobTraceFile.Value().c_str(), O_RDWR | O_CREAT | O_TRUNC, 0666);
-	if (LogFD < 0) {
-		perror("Unable to open trace file");
-		return 1;
-	}
-	
+			
 	PIN_StartProgram();
 	return 0;
 }
